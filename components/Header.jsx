3df41bec84905ce16c0b589d2da9a0aa16f60d2e
JUIN TAN <tanjuinlong@gmail.com>
2024-06-24 13:08:44 +0800

    fix bugs

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import trollFace from "/components/troll-face.png"
 
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./components/troll-face.png"
+                src={trollFace}
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
